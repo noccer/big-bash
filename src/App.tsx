a88fae8a147a56f87b8f9c1900613aa9b1273349
Niall O'Callaghan <noccer@gmail.com>
2017-12-02 03:04:31 +1100

    build app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,69 @@
 import * as React from 'react';
 import './App.css';
+// import '../node_modules/materialize-css/dist/css/materialize.min.css';
 
-const logo = require('./logo.svg');
+import Nav from './Components/Nav/Nav';
+import NavOverlay from './Components/Nav/NavOverlay';
+import Body from './Components/Body/Body';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import {matchData} from './Data/Data';
+import {Match} from './Data/Matches';
+
+const AppPropsData = {
+    // tslint:disable-next-line
+    title: "WhenIsBigBashOn.com"
+};
+
+interface AppProps {
+
 }
 
-export default App;
+interface AppState {
+    navOpen: boolean;
+    matches: Match[];
+    selectedMatch: Match;
+}
+export default class App extends React.Component <AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+        this.state = {
+            navOpen: false,
+            matches: matchData,
+            selectedMatch: matchData[6]
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.selectMatch = this.selectMatch.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <Nav
+                    onToggleNav={this.toggleNav}
+                    navOpen={this.state.navOpen}
+                    matches={this.state.matches}
+                    onSelectMatch={this.selectMatch}
+                    selectedMatch={this.state.selectedMatch}
+                />
+                <Body
+                    match={this.state.selectedMatch}
+                    title={AppPropsData.title}
+                />
+                <NavOverlay onClick={this.toggleNav} navOpen={this.state.navOpen}/>
+            </div>
+        );
+    }
+
+    private toggleNav() {
+        this.setState({
+            navOpen: !this.state.navOpen
+        });
+    }
+
+    private selectMatch(match: Match) {
+        this.setState({
+            selectedMatch: match
+        });
+        this.toggleNav();
+    }
+}
