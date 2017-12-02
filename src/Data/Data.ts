05b1de86522eed12ba729297602208ae1178d8d5
Niall O'Callaghan <noccer@gmail.com>
2017-12-02 18:05:24 +1100

    Updates

--- src/Data/Data.ts
+++
@@ -1,6 +0,0 @@
-import {
-    Match,
-    MATCHES
-} from './Matches';
-
-export const matchData: Match[] = MATCHES;
--- src/Utils/DateUtil.ts
+++
@@ -1,2 +0,0 @@
-export const UI_DATE_FORMAT = 'Do MMM YYYY';
-export const UI_TIME_FORMAT = 'h:mma';
